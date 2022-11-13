15ba2bb2ddae04a84d0d94896fa94e1cb043fde1
jerald011003 <coninegaming@gmail.com>
2022-11-14 00:05:52 +0800

    asdsa

--- accounts/views.py
+++ accounts/views.py
@@ -19,19 +19,7 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import *
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-
-                return redirect('login')
-
+        form = UserCreationForm()
         context = {'form': form}
         return render(request, 'auth/register.html', context)
 
